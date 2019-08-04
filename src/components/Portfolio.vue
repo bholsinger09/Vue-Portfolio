71c627e3cafa6d1a8779a2d438d7f171af31e51d
bholsinger09 <bholsinger@gmail.com>
2019-08-04 16:11:11 -0600

    building folders and file structure

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="portfolio">
+    <!-- PORTFOLIO SECTION -->
+    <section id="portfolio">
+    
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12  text-center">
+            <h2 class="section-heading">Portfolio</h2>
+          </div>
+        </div>
+    
+        <!-- Portfolio -->
+        <div class="row">
+    
+          <div class="col-sm-6 offset-2 ">
+            <a href="https://codepen.io/bholsinger09/pen/ddENjO" target="_blank"><img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1780758/rsz_1build_dom_tree.png">
+              <div class="caption-content">
+                <h3>Caption 1</h3>
+    
+              </div>
+            </a>
+          </div>
+    
+          <div class="col-sm-6 offset-2">
+            <a href="https://codepen.io/bholsinger09/pen/YeMPMM" target="_blank"><img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1780758/rsz_login_box.png">
+              <div class="caption-content">
+                <h3>Caption 2</h3>
+              </div>
+            </a>
+          </div>
+    
+          <div class="col-sm-6 offset-2 ">
+            <a href=" https://codepen.io/bholsinger09/pen/NyBEgg" target="_blank"><img
+                src="https://s3-us-west-2.amazonaws.com/i.cdpn.io/1780758.NyBEgg.small.0ba13063-fe14-4ed9-8344-d9734e21dea8.png">
+    
+              <div class="caption-content">
+                <h3>Caption 3</h3>
+              </div>
+            </a>
+          </div>
+    
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "portfolio",
+    props: {
+     
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section id="about">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 col-offset-2 text-center">
+            <h2 class="section-heading">About</h2>
+          </div>
+        </div>
+    
+        <div class="row">
+          <div class="col-lg-8 col-lg-offset-2">
+            <p class="contWriting"> My name is Benjamin Holsinger. I go by Ben for short. I have recently been
+              accepted
+              into a 6 month scholarship program for front end web development through Udacity. In the first three
+              months
+              prior, I had learned all the basic parts of Html,
+              CSS, and Javascript. I am now able to create and manage webpages at thier basic level. I plan on doing
+              and
+              learning much more. Below you will find all the projects and coding I have been working on. </p>
+    
+          </div>
+    
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+  // @ is an alias to /src
+
+
+  export default {
+    name: "about",
+    components: {
+      
+    }
+  };
+</script>
